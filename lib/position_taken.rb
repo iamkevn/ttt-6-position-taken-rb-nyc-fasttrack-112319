7fd9c98ceb82018d1b2b008d213f8e43cd01bdac
Kevin Rodriguez <iamkevnrodriguez@gmail.com>
2019-11-25 03:47:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -13,3 +13,12 @@ def position_taken?(board, index)
     return true
   end
 end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  end
+  if board[index] == "X" || "O"
+    return true
+  end
+end
